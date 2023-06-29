456b687040971d3cf6d568279dc9d904aa91b670
gyueunnim <rnrmfrbdms@gmail.com>
2023-06-29 15:07:56 +0900

    add: component scan

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,17 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+// Spring 컨터이너에 컨트롤러를 넣어두고 관리함
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
