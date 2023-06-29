bc7a47156f8da6ffc93d35e189933e4cbb1a4b30
gyueunnim <rnrmfrbdms@gmail.com>
2023-06-29 15:26:04 +0900

    add: configuration file

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -10,8 +10,20 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    // 생성자 주입 -> 가장 권장
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+//    다른 방식 - 필드 주입 -> 별로 안좋음
+//    @Autowired private MemberService memberService;
+
+//    다른 방식 - 세터 주입 -> 중간에 잘못 바뀔 우려
+//    private MemberService memberService;
+//
+//    @Autowired
+//    public MemberController(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
 }
